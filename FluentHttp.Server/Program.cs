1b1374edef326eb5ec659b5844efb6470ebcd262
hakanttek <hakanttek@gmail.com>
2025-08-30 08:26:10 +0200

    Add FluentHttp.Server project with logging support
    Introduces a new project, "FluentHttp.Server", to the solution.
    The project targets .NET 8.0 and includes package references for
    logging. Updates `Program.cs` to configure a logger and start
    an HTTP server, enhancing logging capabilities.

---
+++ FluentHttp.Server/Program.cs
@@ -0,0 +1,13 @@
+﻿using FluentHttp;
+using Microsoft.Extensions.Logging;
+
+using var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder.AddConsole();
+    builder.SetMinimumLevel(LogLevel.Information);
+});
+
+var logger = loggerFactory.CreateLogger<HttpServer>();
+
+var server = new HttpServer("http://localhost:5000/", logger);
+await server.StartAsync();
