89daae44f284b5e04bb3e9480892eee0f92a2096
hakanttek <hakanttek@gmail.com>
2025-08-30 09:58:49 +0200

    Refactor logging and service configuration
    - Updated `FluentHttp.Server.csproj` to add new package references for `Microsoft.Extensions.DependencyInjection` and `Microsoft.Extensions.Logging.Abstractions`.
    - Simplified server creation in `Program.cs` by removing logging setup code and using a single line for server startup.
    - Modified `FluentHttp.csproj` to ensure consistent package references across projects.
    - Changed `Services` from a static `IServiceCollection` to a new `StaticConfiguration` class in `HttpServerInstance.cs`, which encapsulates logging configuration.

--- FluentHttp.Server/Program.cs
+++ FluentHttp.Server/Program.cs
@@ -1,13 +1,3 @@
 ﻿using FluentHttp;
-using Microsoft.Extensions.Logging;
 
-using var loggerFactory = LoggerFactory.Create(builder =>
-{
-    builder.AddConsole();
-    builder.SetMinimumLevel(LogLevel.Information);
-});
-
-var logger = loggerFactory.CreateLogger<HttpServer>();
-
-var server = new HttpServer(logger).ListenOn(5000);
-await server.StartAsync();
+await HttpServer.Create().ListenOn(5000).StartAsync();
--- FluentHttp/HttpServerInstance.cs
+++ FluentHttp/HttpServerInstance.cs
@@ -1,18 +1,36 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace FluentHttp;
 
 public partial class HttpServer
 {
-    internal static IServiceCollection Services = new ServiceCollection();
+    internal static StaticConfiguration Services = new();
 
     private static readonly Lazy<IServiceProvider> LazyProvider = new(() => Services.BuildServiceProvider());
 
     internal static IServiceProvider Provider => LazyProvider.Value;
 
-    public static HttpServer Create(Action<IServiceCollection>? options)
+    public static HttpServer Create(Action<StaticConfiguration>? options = null)
     {
+        options ??= opt => opt.AddLogger();
+        options.Invoke(Services);
         Services.AddFluentHttp();
         return Provider.GetRequiredService<HttpServer>();
+    }
+
+    public class StaticConfiguration : ServiceCollection, IServiceCollection
+    {
+        private IServiceCollection Services => this;
+
+        public void AddLogger(Action<ILoggingBuilder>? configure = null)
+        {
+            configure ??= builder =>
+            {
+                builder.AddConsole();
+                builder.SetMinimumLevel(LogLevel.Information);
+            };
+            Services.AddLogging(configure);
+        }                
     }
 }
