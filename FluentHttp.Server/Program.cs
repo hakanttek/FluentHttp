bb8d8e03aabb342f9a3cfde4ef55a0c6d73059ff
hakanttek <hakanttek@gmail.com>
2025-08-31 21:49:40 +0200

    add FooService as mock

--- FluentHttp.Server/Program.cs
+++ FluentHttp.Server/Program.cs
@@ -1,15 +1,17 @@
 ﻿using FluentHttp;
 using FluentHttp.Attributes;
 using FluentHttp.Models;
+using FluentHttp.Server;
+using Microsoft.Extensions.DependencyInjection;
 using System.Net;
 
-await HttpServer.Create()
-    .Get("/foo", () =>
-    {
-        var res = new { message = "Hello, World!" };
-        return HttpStatusCode.OK.Data(res);
-    })
-    .Get("/bar", ([Query] int baz) => HttpStatusCode.OK.Data(baz))
-    .Fallback(() => HttpStatusCode.NotFound.Data(new { message = "Not found!" }))
-    .ListenOn(5000)
-    .StartAsync();
+await HttpServer.Create(options => options.AddScoped<FooService>())
+.Get("/foo", () =>
+{
+    var res = new { message = "Hello, World!" };
+    return HttpStatusCode.OK.Data(res);
+})
+.Get("/bar", ([Query] int baz) => HttpStatusCode.OK.Data(baz))
+.Fallback(() => HttpStatusCode.NotFound.Data(new { message = "Not found!" }))
+.ListenOn(5000)
+.StartAsync();
