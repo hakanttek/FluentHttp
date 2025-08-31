c088e8a2b791dea9e60778d50e621dbd2118dc65
hakanttek <hakanttek@gmail.com>
2025-08-31 22:07:45 +0200

    add post example

--- FluentHttp.Server/Program.cs
+++ FluentHttp.Server/Program.cs
@@ -5,13 +5,22 @@ using FluentHttp.Server;
 using Microsoft.Extensions.DependencyInjection;
 using System.Net;
 
-await HttpServer.Create(options => options.AddScoped<FooService>())
+await HttpServer.Create(options =>
+{
+    options.AddScoped<FooService>();
+    options.AddLogger();
+})
 .Get("/foo", async (FooService service, [Query] int? bar, [Query] string? qux) =>
 {
     var foos = await service.GetAsync(bar, qux);
     return foos.Any() 
         ? HttpStatusCode.OK.Data(foos)
         : HttpStatusCode.NotFound.Data("No foos found!");
+})
+.Post("/foo", async (FooService service, [Body] Foo foo) =>
+{
+    await service.AddAsync(foo);
+    return HttpStatusCode.Created;
 })
 .Fallback(() => HttpStatusCode.NotFound.Data(new { message = "Not found!" }))
 .ListenOn(5000)
