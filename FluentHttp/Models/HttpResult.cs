e273c8048218147011f7f77d8550eeb16cf11623
hakanttek <hakanttek@gmail.com>
2025-08-31 13:22:15 +0200

    feat(HttpResult): add HttpResult record and Data extension method for HttpStatusCode

---
+++ FluentHttp/Models/HttpResult.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace FluentHttp.Models;
+
+public record HttpResult(int StatusCode, object? Data = null);
+
+public static class Extensions
+{
+    public static HttpResult Data(this HttpStatusCode statusCode, object data) => new ((int)statusCode, data);
+}
