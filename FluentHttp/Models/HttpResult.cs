b63ca69f0e59d54d251807b023502e8aec60f041
hakanttek <hakanttek@gmail.com>
2025-08-31 13:38:14 +0200

    refactor(HttpResult): add constructure to be able to create with HttpStatusCode-enum

--- FluentHttp/Models/HttpResult.cs
+++ FluentHttp/Models/HttpResult.cs
@@ -2,7 +2,13 @@
 
 namespace FluentHttp.Models;
 
-public record HttpResult(int StatusCode, object? Data = null);
+public record HttpResult(int StatusCode, object? Data = null)
+{
+    public HttpResult(HttpStatusCode statusCode, object? data = null)
+        : this((int)statusCode, data)
+    {
+    }
+}
 
 public static class Extensions
 {
